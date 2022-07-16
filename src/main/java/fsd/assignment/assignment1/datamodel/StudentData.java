c3bd66555dde899f3bf4f35b13fd0925a150fbb8
zxszahra <108396318+zxszahra@users.noreply.github.com>
2022-07-16 19:14:25 +0100

    Update StudentData.java

--- src/main/java/fsd/assignment/assignment1/datamodel/StudentData.java
+++ src/main/java/fsd/assignment/assignment1/datamodel/StudentData.java
@@ -12,22 +12,16 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class StudentData {
-    /* TODO: include an instance of StudentData
-    */
     private static StudentData instance = new StudentData();
     private static String filename = "student-data.txt";
 
     private ObservableList<Student> students;
 
     public static StudentData getInstance() {
-        /* TODO: complete the getter for the instance created
-        */
         return instance;
     }
 
     public ObservableList<Student> getStudents() {
-        /* TODO: complete the getter for the observable arraylist
-        */
         return students;
     }
 
@@ -40,23 +34,13 @@ public class StudentData {
 
         try {
             while ((input = br.readLine()) != null) {
-                /* TODO: split each input line using a tab
-                 */
                 String[] studentItem = input.split("\t");
-                /* TODO: using the String create each piece of data so that all the instance variables
-                         have a value accordingly
-                 */
                 String studId = studentItem[0];
                 String yearStudy = studentItem[1];
                 String mod1 = studentItem[2];
                 String mod2 = studentItem[3];
                 String mod3 = studentItem[4];
-                /* TODO: complete the call to the constructor by passing in the parameters
-                 */
                 Student studDataItem = new Student(studId, yearStudy, mod1, mod2, mod3);
-                /* TODO: add the studentDataItem to the students array
-                 */
-                //>include the statement here
                 students.add(studDataItem);
             }
         } finally {
@@ -70,26 +54,15 @@ public class StudentData {
         Path path = Paths.get(filename);
         BufferedWriter bw = Files.newBufferedWriter(path);
         try {
-            /* TODO: complete the iterator
-             */
             Iterator<Student> it = students.iterator();
             while (it.hasNext()) {
-                /* TODO: accept the next item from the iterated list
-                 */
                 Student item = it.next();
-                /* TODO: complete the write() using String.format
-                         remember to separate each string with a tab
-                 */
                 bw.write(String.format("%s\t%s\t%s\t%s\t%s",
                         item.getStudId(),
                         item.getYearOfStudy(),
                         item.getModule1(),
                         item.getModule2(),
                         item.getModule3()));
-                /* TODO: once a student item is written to the file ensure that
-                         the next item is stored on a new line
-                 */
-                //>insert statement here
                 bw.newLine();
             }
         } finally {
@@ -100,15 +73,9 @@ public class StudentData {
     }
 
     public void addStudentData(Student studentToAdd){
-        /* TODO: complete the addStudentData so that a student can be added
-                 to students
-         */
         students.add(studentToAdd);
     }
     public void deleteStudent(Student stu){
-        /* TODO: complete the addStudentData so that a student can be removed
-                 from students
-         */
         students.remove(stu);
     }
 }
